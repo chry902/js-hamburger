e9ebc6039539b8d34fcae8646907194d23f880f9
chry902 <tchry902@gmail.com>
2021-10-12 19:16:34 +0200

    avanzamento

--- main.js
+++ main.js
@@ -3,5 +3,11 @@
 
 
 
-
+/*
 document.querySelector(".hamburger-menu").style.display = "block";
+*/
+
+
+document.querySelector(".hamburger-menu").addEventListener("click", function () {
+    document.querySelector(".hamburger-menu ").style.display = "block";
+});
